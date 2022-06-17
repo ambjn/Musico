508cc9fe62cb54d1919171b822acd2f558047ef2
Amber Jain <jainanuamber@gmail.com>
2022-06-17 20:25:25 +0530

    updated

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:musico/screens/home_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -12,9 +13,9 @@ class SplashScreen extends StatelessWidget {
             children: [
               Container(
                 alignment: Alignment.bottomLeft,
-                padding: const EdgeInsets.only(top: 75, right: 25),
+                padding: const EdgeInsets.only(top: 75, left: 25),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     const Text(
                       "muSi",
@@ -31,46 +32,56 @@ class SplashScreen extends StatelessWidget {
                   ],
                 ),
               ),
-              Container(
-                alignment: Alignment.bottomLeft,
-                padding: const EdgeInsets.only(top: 20, left: 20, right: 100),
-                child: Image.asset(
-                  'assets/images/music.png',
-                ),
-              ),
               const SizedBox(
-                height: 40,
+                height: 20,
               ),
-              Container(
-                width: 150.0,
-                height: 50.0,
-                child: ElevatedButton(
-                    onPressed: () {
-                      // await Navigator.of(context).push(
-                      //     MaterialPageRoute(
-                      //         builder: (_) => const NotesScreen()));
-                    },
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(
-                            Colors.orangeAccent.shade400),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                        ))),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        Text(
-                          "Let's Start",
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        Icon(
-                          Icons.arrow_forward,
-                          color: Colors.white,
-                        ),
-                      ],
-                    )),
+              Stack(
+                children: [
+                  Container(
+                    padding:
+                        const EdgeInsets.only(top: 20, left: 20, right: 100),
+                    child: Image.asset(
+                      'assets/images/music.png',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Positioned(
+                    bottom: 25,
+                    right: 20,
+                    child: Container(
+                      width: 150.0,
+                      height: 50.0,
+                      child: ElevatedButton(
+                          onPressed: () async {
+                            await Navigator.of(context).push(MaterialPageRoute(
+                                builder: (_) => const HomeScreen()));
+                          },
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                  Colors.orangeAccent.shade400),
+                              shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30.0),
+                              ))),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: const [
+                              Text(
+                                "Let's Start",
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              Icon(
+                                Icons.arrow_forward,
+                                color: Colors.white,
+                              ),
+                            ],
+                          )),
+                    ),
+                  ),
+                ],
               ),
               Expanded(
                 child: Container(
